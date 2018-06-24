bd92a0b0a94c1198fd12bc1c9f0eac53b8afae22
Alexandre Sullet <alexandresullet@hotmail.com>
2018-06-24 22:30:17 +0200

    API added

---
+++ api.py
@@ -0,0 +1,108 @@
+from flask import Flask, request, jsonify
+from flask_restful import reqparse, abort, Api, Resource
+import datetime as dt
+import numpy as np
+import pandas as pd
+import time
+from flask_cors import CORS, cross_origin
+from pymongo import MongoClient
+import pymongo
+import pandas as pd
+import unicodedata
+import csv
+
+app = Flask(__name__)
+CORS(app)
+
+data = pd.read_csv("dataTimeUse.csv", sep = ",", index_col = 0)
+
+@app.route('/Homepage', methods = ['GET', 'POST'])
+def getDataHomePage():
+
+	# Argument part for API calls
+	
+	# country = request.args['country'].title()
+	# daysweek = request.args['daysweek']
+	# sex = request.args['sex']
+	# categories = request.args['categories']
+
+	# TEST PART 
+	country = 'France'
+	daysweek = 'All days of the week'
+	sex = 'Males'
+	categories = ['Eating', 'Household and family care', "Leisure, social and associative life"]
+
+	results2000 = data[(data['YEAR'] == 2000) & (data['COUNTRY'] == country) & (data['DAYSWEEK'] == daysweek) & (data['SEX'] == sex) & (data['CATEGORY'].isin(categories))]
+	results2010 = data[(data['YEAR'] == 2010) & (data['COUNTRY'] == country) & (data['DAYSWEEK'] == daysweek) & (data['SEX'] == sex) & (data['CATEGORY'].isin(categories))]
+
+	# 2000 Part
+
+	timeSpent2000 = list(results2000['SUM Time spent (hh:mm)'])
+
+	for i in range(len(timeSpent2000)):
+		h, m = timeSpent2000[i].split(":")
+		partTime = int(h) + int(m) / 60
+		timeSpent2000[i] = round(partTime, 1)
+
+	participationTime2000 = list(results2000['SUM Participation time (hh:mm)'])
+
+	for i in range(len(participationTime2000)):
+		h, m = participationTime2000[i].split(":")
+		partTime = int(h) + int(m) / 60
+		participationTime2000[i] = round(partTime, 1)
+
+	participationRate2000 = list(round(results2000['MEAN Participation rate (%)'], 1))
+
+	#  2010 Part
+
+	timeSpent2010 = list(results2010['SUM Time spent (hh:mm)'])
+
+	for i in range(len(timeSpent2010)):
+		h, m = timeSpent2010[i].split(":")
+		partTime = int(h) + int(m) / 60
+		timeSpent2010[i] = round(partTime, 1)
+
+	participationTime2010 = list(results2010['SUM Participation time (hh:mm)'])
+
+	for i in range(len(participationTime2010)):
+		h, m = participationTime2010[i].split(":")
+		partTime = int(h) + int(m) / 60
+		participationTime2010[i] = round(partTime, 1)
+
+	participationRate2010 = list(round(results2010['MEAN Participation rate (%)'], 1))
+
+	try:
+		answer = {
+					'Year2000' : 
+						{
+							'Country' : country,
+							'Daysweek' : daysweek,
+							'Sex' : sex,
+							'Categories' : categories,
+							'Time spent' : timeSpent2000,
+							'Participation time' : participationTime2000,
+							'Participation rate' : participationRate2000,
+						},
+					'Year2010' : 
+						{
+							'Country' : country,
+							'Daysweek' : daysweek,
+							'Sex' : sex,
+							'Categories' : categories,
+							'Time spent' : timeSpent2010,
+							'Participation time' : participationTime2010,
+							'Participation rate' : participationRate2010,
+						}
+				}
+
+	except:
+		answer = {
+					'Error' : 'No Data'
+				}
+
+	return jsonify(answer)
+
+if __name__ == '__main__':
+	app.run(debug = True)
+
+	
